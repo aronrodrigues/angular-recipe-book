147abb6e1c09a7473d054e44855cb5af885443cc
Rodrigues <aronr@amazon.com>
2018-02-11 13:12:15 +0000

    Section 8: Course Project - Directives

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, HostListener, ElementRef, Renderer2, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+
+  constructor(private elementRef : ElementRef, private renderer : Renderer2) { }
+
+  @HostBinding("class.open") isOpen = false;
+
+  @HostListener("click") toggleOpen() {
+    /*if (!this.isOpen) {
+      this.renderer.addClass(this.elementRef.nativeElement, "open");
+    } else {
+      this.renderer.removeClass(this.elementRef.nativeElement, "open");
+    }*/
+    this.isOpen = !this.isOpen;
+  }
+
+}
