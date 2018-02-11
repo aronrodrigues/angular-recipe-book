ad52b69f8d0722285cd92e12a6d65e845e86ff14
Rodrigues <aronr@amazon.com>
2018-02-11 11:21:18 +0000

    Section 6: Course Project - Components & Databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -7,17 +7,25 @@ import { Recipe } from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  @Output() onRecipeSelectedOnItem = new EventEmitter<Recipe>()
   recipes: Recipe[] = [
     new Recipe(
       'Filet au poivre',
       'Filet com molho de pimenta e pure de batata',
-      'https://panelinha-sitenovo.s3-sa-east-1.amazonaws.com/receita/963889200000-Filet-au-poivre.jpg')
+      'https://panelinha-sitenovo.s3-sa-east-1.amazonaws.com/receita/963889200000-Filet-au-poivre.jpg'),
+      new Recipe(
+        'Filet a Oswaldo Aranha',
+        'Filet frito na manteiga',
+        'http://www.blogangeloni.com.br/wp-content/uploads/2015/08/p38-39-1024x1024.jpg')
   ]
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectRecipe(recipe) {
+    this.onRecipeSelectedOnItem.emit(recipe)
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -11,7 +11,9 @@ export class ShoppingListComponent implements OnInit {
     new Ingredient("Apples", 5),
     new Ingredient("Tomatoes", 2)
   ];
-  
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
   constructor() { }
   
   ngOnInit() {
